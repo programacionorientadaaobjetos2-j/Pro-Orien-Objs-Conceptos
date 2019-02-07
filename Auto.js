7286ce3b547edf25ee7c6b1c0883d1854d1721d1
rcarrasco3652 <rcarrasco0@ucol.mx>
2019-02-07 08:35:12 -0600

    Trabajo em clase

---
+++ Auto.js
@@ -0,0 +1,9 @@
+class Auto {
+    constructor(marca, modelo, color, kilometraje){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.color=color;
+        this.kilometraje=kilometraje;
+        this.estado="Apagado"
+    }
+}
