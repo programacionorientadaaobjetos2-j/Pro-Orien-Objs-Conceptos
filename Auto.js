163de1af036e0334c7f99870d997a41ab41ff81d
rcarrasco3652 <rcarrasco0@ucol.mx>
2019-02-08 15:29:20 -0600

    Trabajo en clase 08/02/2019

--- Auto.js
+++ Auto.js
@@ -1,9 +1,45 @@
-class Auto {
-    constructor(marca, modelo, color, kilometraje){
-        this.marca=marca;
-        this.modelo=modelo;
-        this.color=color;
-        this.kilometraje=kilometraje;
-        this.estado="Apagado"
+ export default class Auto {
+    constructor(marca, modelo, color="blanco", kilometraje=0){
+        this._marca=marca;
+        this._modelo=modelo;
+        this._color=color;
+        this._kilometraje=kilometraje;
+        this._estado="Apagado";
     }
+
+    get color(){
+        return this._color; //Convertir un atributo en solo lectura
+
+    }
+
+    set color(color){
+        this._color=color; //hacer un atributo en solo escritura
+        
+    }
+
+    mostrarEstado() {
+        console.log('El auto ' + this._marca + ' ' + this._color + ' del año ' + this._modelo);
+        
+        console.log( `El auto ${this._marca} ${this._color} del año ${this._modelo} tiene un 
+        kilometraje de ${this._kilometraje}kms y esta ${this._estado}`);
+    }
+    encender() {
+        this._estado="Encendido";
+        this.mostrarEstado();
+    }
+    apagar() {
+        this._estado="Apagado";
+        this.mostrarEstado();
+    }
+    avanzar(velocidad, tiempo) {
+        if(this._estado==="Apagado"){
+            console.log(`El auto no puede avanzar por que esta apagado`)
+        }
+        else{
+            let distancia=velocidad*tiempo;
+            this._kilometraje= this._kilometraje + distancia;
+            this._mostrarEstado;
+        }
+    }
+
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hola, Mundo!!")
+import Auto from "./Auto.js";
+
+let auto1 = new Auto("Ford", 200, "Rojo", 100);
+auto1.mostrarEstado();
+auto1.encender();
+auto1.avanzar(50, 2);
+auto1.apagar();
+auto1.avanzar(20,2);
+
+let auto2 = new Auto("Nissan", 2018, "blanco");
+auto2.encender();
+let auto3 = new Auto("vw", 2010);
+auto3.encender();
+
+console.log(`Color ${auto1.color}`); //Lectura
+console.log(`Color ${auto2.color}`); //Lectura
+auto1.color="Amarillo"; //Escritura
+auto2.color="Azul";  //Escritura
