07952015dc3c84e9d734562e21f15833c2ca8288
rcarrasco3652 <rcarrasco0@ucol.mx>
2019-02-27 19:31:29 -0600

    Trabajo en clase 27/02/2019

---
+++ boostrap/Empleado.js
@@ -0,0 +1,10 @@
+export default class Empleado{
+    constructor(nombre,email,birthday){
+        this._name=nombre;
+        this._email=email;
+        this._birthday=birthday;
+    }
+    getAge(){
+        
+    }
+}
